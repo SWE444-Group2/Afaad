7b378d9d47e10dd47b9dc0772694934ed6511c7c
Lujain Almutairi <51900114+LujainAb@users.noreply.github.com>
2021-09-15 21:27:50 +0300

    Added Product details and investor details

--- App.js
+++ App.js
@@ -3,8 +3,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { default as splash } from './screens/splash' ;
 import { default as ViewIdea } from './screens/ViewIdea' ;
-
-
+import { default as productIdea } from './screens/productIdea' ;
+import {default as invstorsAccount} from './screens/investorAccount';
 import { default as Login } from './screens/Login' ;
 import { default as welcome } from './screens/welcome' ;
 
@@ -15,6 +15,8 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="splash">
       <Stack.Screen name="View Idea" component={ViewIdea}/>
+        <Stack.Screen name="Product Idea Details" component={productIdea}/>
+        <Stack.Screen name="investor Account Details" component={invstorsAccount}/>
         <Stack.Screen name="splash" component={splash} />       
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="welcome" component={welcome} options={{ headerBackVisible: false, gestureEnabled: false, }}/>
