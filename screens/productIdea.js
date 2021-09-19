ea37f6adeaa91f99e0f056532bdacf0010695242
Lujain Almutairi <51900114+LujainAb@users.noreply.github.com>
2021-09-20 00:29:14 +0300

    Updated Attributes to view

--- screens/productIdea.js
+++ screens/productIdea.js
@@ -26,17 +26,9 @@ export default function productIdea({navigation , route}) {
         setDescription(snapshot.child("ProductDescription").val());
         setVerified(snapshot.child("accepted").val());
         setcategory(snapshot.child("category").val());
-        /*----
-         why showing verified in  the details?
-         -----setInvested(snapshot.child("invested").val());
-         no need since the idea is still not accepted
-         -----setFeasibilityAnalysis(snapshot.child("FeasibilityAnalysis").val());
-         do not know how to upload the analysis yet 
-         setTitle(snapshot.child("Title").val());
         setcostEstimation(snapshot.child("costEstimation").val());
         setinvestorsSpec(snapshot.child("investorsSpec").val());
-        setUserID(snapshot.child("userID"))
-          */ });
+       });
 
     const AcceptIdea=()=>{
 
@@ -68,18 +60,14 @@ export default function productIdea({navigation , route}) {
     // console.log(pIdea);
     return(
         <View style={styles.container}>
-           {/* <Text style={styles.label}>Feasibility Analysis</Text>
-            <Text>{FeasibilityAnalysis}</Text>       
-            <Text style={styles.label}>Cost Estimation</Text>
-            <Text>{costEstimation}</Text>
+          <Text style={styles.label}>Title</Text>
+            <Text>{Title}</Text> 
+            <Text style={styles.label}>Description</Text>
+            <Text>{Description}</Text> 
             <Text style={styles.label}>Cost Estimation</Text>
             <Text>{costEstimation}</Text>
             <Text style={styles.label}>Investors Specifications</Text>
-            <Text>{investorsSpec}</Text>*/}
-            <Text style={styles.label}>Title</Text>
-            <Text>{Title}</Text>
-            <Text style={styles.label}>Description</Text>
-            <Text>{Description}</Text>
+            <Text>{investorsSpec}</Text>
             <Text style={styles.label}>Date</Text>
             <Text>{date}</Text>
             <Text style={styles.label}>Catagory</Text>
